5941f642668c0e5d32d4c3cce530d5f142cfd1a4
riccardotreso <riccardo.treso@hotmail.com>
2014-02-14 11:49:57 +0100

    First Commit

---
+++ Default.aspx
@@ -0,0 +1,194 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DropDownList.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <script src="Scripts/jquery-2.1.0.js"></script>
+    <script src="jquery.dropdown.js"></script>
+    <script>
+        var list = [{
+            id: 1,
+            nome: "mario",
+            cognome: "rossi"
+        },
+        {
+            id: 2,
+            nome: "giovanni",
+            cognome: "bianchi"
+        },
+        {
+            id: 3,
+            nome: "maria",
+            cognome: "verdi"
+        }]
+        $(document).ready(function () {
+
+
+            $('#ddl1').dropdown({
+                popUpWidth: 700,
+                popUpHeight: 500,
+                dataSource: {
+                    data: list
+                },
+                dataTextField: "nome",
+                dataValueField: "id",
+                columns: [{
+                    field: "id",
+                    width: 166,
+                    hidden: true,
+                    title: "ID"
+                },
+                {
+                    field: "nome",
+                    width: 166,
+                    hidden: false,
+                    title: "nome"
+                },
+                {
+                    field: "cognome",
+                    width: 166,
+                    hidden: false,
+                    title: "cognome"
+                }]
+            });
+
+            $('#Text1').dropdown({
+                popUpWidth: 100,
+                popUpHeight: 700
+            });
+
+            $('#Text2').dropdown();
+
+            $('#Text3').dropdown();
+        });
+
+
+        Test = function () {
+            var r = $('#ddl1').dropdown("getSelectedItem");
+            alert(r);
+            return false;
+        }
+
+    </script>
+    <style>
+        table.result
+        {
+            width: 100%;
+            padding: 0;
+        }
+
+            table.result thead tr th
+            {
+                background-image: url(img/ui-bg_highlight-soft_75_cccccc_1x100.png);
+                background-repeat: repeat-x;
+            }
+
+            table.result tr
+            {
+                background-color: white;
+            }
+
+                table.result tr:hover
+                {
+                    background-color: silver;
+                }
+
+            table.result td
+            {
+                border: solid 1px silver;
+            }
+
+        .divContainer
+        {
+            /*float: left;*/
+            /*clear: both;*/
+            overflow: hidden;
+            position: relative;
+            display: inline-block;
+        }
+
+        .divResult
+        {
+            border: solid 1px silver;
+            background-color: white;
+            display: none;
+            position: absolute;
+            z-index: 100000;
+            margin-top: -4px;
+        }
+
+        .spaninput
+        {
+            float: left;
+            height: 19px;
+            width: 150px;
+            border-right-style: none;
+            overflow: hidden;
+            position: relative;
+            display: inline-block;
+        }
+
+        .icon
+        {
+            width: 23px;
+            height: 21px;
+            background-image: url(img/ui-icons_666666_256x240.png);
+            float: left;
+            position: relative;
+            overflow: hidden;
+            display: inline-block;
+            border: solid 1px silver;
+        }
+
+            .icon:hover
+            {
+                background-color: #EEEEEE;
+            }
+
+        .arrow
+        {
+            background-position: -61px -12px;
+        }
+    </style>
+
+    <form id="form1" runat="server">
+        <div>
+
+            <table>
+                <tr>
+                    <td>
+                        <span>DDL1</span>
+                        <input type="text" id="ddl1" />
+                        <input type="submit" value="getSelectedItem" onclick="return Test();" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span>DDL2</span>
+                        <input type="text" id="Text1" />
+                    </td>
+                </tr>
+            </table>
+
+
+
+            <input type="text" id="Text2" />
+
+            <input type="text" id="Text3" />
+
+
+            
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ jquery.dropdown.js
@@ -0,0 +1,233 @@
+﻿/// <reference path="script\jquery-2.1.0.js" />
+
+
+(function ($) {
+
+
+    /* Invoke the dropdown functionality.
+	   @param  options  string - a command, optionally followed by additional parameters or
+						Object - settings for attaching new datepicker functionality
+	   @return  jQuery object */
+    $.fn.dropdown = function (options) {
+
+
+        if (!$.dropdown.initialized) {
+            $(document).mousedown($.dropdown._checkExternalClick);
+            $.dropdown.initialized = true;
+        }
+
+        if (typeof options === "string" && (options === "getSelectedItem")) {
+            return $.dropdown["_" + options + "DropDownList"].apply($.dropdown, this);
+        }
+
+        return this.each(function () {
+            $.dropdown._attachDropDown(this, options);
+        });
+    };
+
+
+
+
+    function DropDownList() {
+        this._openDiv = "DDL_DIVOPEN";
+        this._instance = "DDL_instance";
+        this._selectedItem = "DDL_SelectedItem";
+        this.popUpWidth = 400;
+        this.popUpHeight = 100;
+    };
+
+    $.extend(DropDownList.prototype, {
+
+        
+
+        /* Attach the drop-down list to a jQuery selection.
+		 * @param  target	element - the target input field or division or span
+		 * @param  settings  object - the new settings to use for this date picker instance (anonymous)
+		 */
+        _attachDropDown: function (target, settings) {
+            var nodeName, inline, inst;
+            var divContainer, divOuter, parentTargetNode, imagesArrow;
+            nodeName = target.nodeName.toLowerCase();
+            
+
+            if (nodeName === "input") {
+
+                parentTargetNode = $(target).parent();
+                divContainer = $('<div>');
+                divOuter = $('<div>');
+                imagesArrow = $('<span>');
+
+                $(target).attr("class", "spaninput");
+                divOuter.attr("class", "divOuter");
+                divContainer.attr("class", "divContainer");
+                imagesArrow.attr("class", "icon arrow");
+
+                divOuter.appendTo(parentTargetNode);
+                $(target).appendTo(divContainer);
+                imagesArrow.appendTo(divContainer);
+                divContainer.appendTo(divOuter);
+
+
+                var ddl = this._newInst($(target), divOuter, divContainer);
+                ddl = $.extend(ddl, settings || {});
+                this._insData(target, this._instance, ddl);
+
+                //bind event 
+                $(target).keyup(function (event) {
+                    if (event.keyCode === 13) {
+                        $.dropdown._showResult(target);
+                    }
+                });
+
+                imagesArrow.click(function (event) {
+                    $.dropdown._showResult(target);
+                });
+
+            }
+        },
+
+        _newInst: function (target, divOuter, divContainer) {
+            
+            return {
+                input: target,
+                divOuter: divOuter,
+                divContainer: divContainer
+            }
+        },
+
+        _selectIndexChanged: function (object, target) {
+
+            this._removeData(target, this._selectedItem);
+            this._insData(target, this._selectedItem, object);
+            var instance = this._getData(target, this._instance);
+            target.value = object[instance["dataTextField"]];
+        },
+
+        _getSelectedItemDropDownList: function (target) {
+            var selectedItem = this._getData(target, this._selectedItem);
+            return selectedItem;
+        },
+
+        /* 
+		 * @param  input  element 
+		 */
+        _showResult: function (input) {
+            input = input.target || input;
+            var parent, divResult;
+            parent = $(input).parent().parent();
+
+
+
+            var instance = this._getData(input, this._instance);
+
+            divResult = parent.find("div.divResult");
+            if (divResult.length === 0) {
+                divResult = $('<div>');
+                divResult.attr("class", "divResult");
+                divResult.width(instance.popUpWidth ? instance.popUpWidth : 400);
+                divResult.height(instance.popUpHeight ? instance.popUpHeight : 100);
+                divResult.appendTo(parent);
+
+                if (instance.dataSource)
+                {
+                    var data = instance.dataSource.data;
+                    var colums = instance.columns;
+
+                    var table, thead, trHead, tbody, trBody;
+
+                    table = $("<table class='result' cellspacing=0 cellpadding=0>");
+                    //thead
+                    thead = $("<thead>");
+                    trHead = $("<tr>");
+                    $(colums).each(function (index, object) {
+                        if (object["hidden"] === false) {
+                            var th = $("<th>");
+                            th.html(object["title"]);
+                            th.appendTo(trHead);
+                        }
+                    });
+                    trHead.appendTo(thead);
+                    thead.appendTo(table);
+                   
+                    //tbody
+                    tbody = $("<tbody>");
+                    $(data).each(function (index, objData) {
+                        trBody = $("<tr>");
+                        
+                        $(colums).each(function (index, objColumns) {
+                            if (objColumns["hidden"] === false)
+                            {
+                                var td = $("<td>");
+                                td.html(objData[objColumns["field"]]);
+
+                                td.mousedown(function (event) {
+                                    $.dropdown._selectIndexChanged(objData, input);
+                                });
+
+                                td.appendTo(trBody);
+
+                                
+                            }
+                        });
+                        
+
+                        trBody.appendTo(tbody);
+                        
+                        
+                    });
+                    tbody.appendTo(table);
+
+                    table.appendTo(divResult);
+                    
+                }
+
+            }
+            divResult.show();
+
+
+            this._insData(document, $.dropdown._openDiv, divResult);
+
+
+
+
+        },
+
+        _insData: function (target, key, value) {
+            return $.data(target, key, value);
+        },
+
+        _getData: function (target, key) {
+            return $.data(target, key);
+        },
+
+        _removeData: function (target, key, value) {
+            return $.removeData(target, key);
+        },
+
+        _checkExternalClick: function (event) {
+            //if (!$.datepicker._curInst) {
+            //	return;
+            //}
+
+            //var input = target;
+            var objDiv = $.dropdown._getData(document, $.dropdown._openDiv);
+            try {
+                if (objDiv) {
+                    //event.stopPropagation();
+                    $(objDiv).hide();
+                    $.dropdown._removeData(document, $.dropdown._openDiv);
+                }
+            }
+            catch (e)
+            { }
+
+        }
+
+    });
+
+    $.dropdown = new DropDownList();
+    $.dropdown.initialized = false;
+    $.dropdown.uuid = new Date().getTime();
+    $.dropdown.version = "1.0.0";
+
+})(jQuery);
